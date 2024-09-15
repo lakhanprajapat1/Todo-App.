cd503f17f8f40aa60f7ccda014d712a197037fe2
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-15 13:28:29 +0530

    Add files via upload

---
+++ todoapp.js
@@ -0,0 +1,31 @@
+// let todo=[];
+
+// let req=prompt("Enter Your Request");
+
+// while (true) {
+//     if(req=="quit"){
+//         console.log("Quiting the aap");
+//         break;
+//     }
+//     if(req=="list"){
+//         console.log("_________________");
+//         for(let i=0; i<todo.length; i++){
+//             console.log(i, todo[i]);
+//         }
+//         console.log("_________________");
+//     }
+//     else if (req=="add"){
+//        let task = prompt("Enter you want to add");
+//        todo.push(task)
+//        console.log("task added");
+//     }
+//     else if(req=="delete"){
+//        let idx= prompt("Enter index of you want to delete task");
+//        todo.splice(idx, 1)
+//        console.log(" task deleted")
+//     }else{
+//         console.log("Wrong Request");
+//     }
+
+//     req = prompt("Please enter your task");
+// } 
