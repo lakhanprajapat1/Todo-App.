1c74aecad10d4d6768018a51efe36bfade155955
Lakhan Prajapat <lakhan10209@gmail.com>
2024-09-15 13:29:14 +0530

    Update todoapp.js

--- todoapp.js
+++ todoapp.js
@@ -1,28 +1,28 @@
-// let todo=[];
+let todo=[];
 
-// let req=prompt("Enter Your Request");
+let req=prompt("Enter Your Request");
 
-// while (true) {
-//     if(req=="quit"){
-//         console.log("Quiting the aap");
-//         break;
-//     }
-//     if(req=="list"){
-//         console.log("_________________");
-//         for(let i=0; i<todo.length; i++){
-//             console.log(i, todo[i]);
-//         }
-//         console.log("_________________");
-//     }
-//     else if (req=="add"){
-//        let task = prompt("Enter you want to add");
-//        todo.push(task)
-//        console.log("task added");
-//     }
-//     else if(req=="delete"){
-//        let idx= prompt("Enter index of you want to delete task");
-//        todo.splice(idx, 1)
-//        console.log(" task deleted")
+while (true) {
+    if(req=="quit"){
+        console.log("Quiting the aap");
+        break;
+    }
+    if(req=="list"){
+        console.log("_________________");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("_________________");
+    }
+    else if (req=="add"){
+       let task = prompt("Enter you want to add");
+       todo.push(task)
+       console.log("task added");
+    }
+    else if(req=="delete"){
+       let idx= prompt("Enter index of you want to delete task");
+       todo.splice(idx, 1)
+       console.log(" task deleted")
 //     }else{
 //         console.log("Wrong Request");
 //     }
